1bfb2a4ab136c48005f457de068c7ee03bf1b594
Development <Development@Brians-MacBook-Pro.local>
2018-08-04 22:26:43 -0400

    clean up code

--- main.go
+++ main.go
@@ -15,20 +15,20 @@ import (
 //GET /index
 func index(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		posts := []post.Post{}
 
 		for i := 1; i <= 20; i++ {
-			p := post.Post{}
-			p.ID = i
-			p.Title = lorem.Sentence(1, 10)
-			p.Text = lorem.Paragraph(4, 6)
-			p.Source = lorem.Host()
-			p.Author = randomdata.FirstName(randomdata.RandomGender)
-			p.DateTimeCreated = randomdata.FullDate()
+			p := post.Post{
+				ID:              i,
+				Title:           lorem.Sentence(1, 10),
+				Text:            lorem.Paragraph(4, 6),
+				Author:          randomdata.FirstName(randomdata.RandomGender),
+				DateTimeCreated: randomdata.FullDate(),
+			}
 			posts = append(posts, p)
 		}
 
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		b, err := json.Marshal(posts)
 		util.Check(err)
 		fmt.Fprintf(w, string(b[:]))
@@ -36,13 +36,9 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//Route for favicon - Google Chrome fix - calls /index route twice
-func handlerICon(w http.ResponseWriter, r *http.Request) {}
-
 //Initialize Server with Routes
 func main() {
 	fmt.Println("Running local server @ http://localhost:" + os.Getenv("PORT"))
-	http.HandleFunc("/favicon.ico", handlerICon)
 	http.HandleFunc("/data.json", index)
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
--- pkg/post/post.go
+++ pkg/post/post.go
@@ -4,7 +4,6 @@ type Post struct {
 	ID              int    `json:"id"`
 	Title			string `json:"title"`
 	Text            string `json:"text"`
-	Source          string `json:"source"`
 	Author          string `json:"username"`
 	DateTimeCreated string `json:"datetimecreated"`
 }
